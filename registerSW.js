e852d2faa47ac678cd00132d71ae4ac61feae6a6
NimaMoobed <nimamoobed@users.noreply.github.com>
2026-06-02 15:40:55 +0330

    Custom domain deploy

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/negragarigame/sw.js', { scope: '/negragarigame/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
